0d8833e80d6a16d433011c525b73a5b68e40ae42
JamesParrott <80779630+JamesParrott@users.noreply.github.com>
2024-01-25 15:54:28 +0000

    Test Alpine image

--- test_windows_dockerfile.py
+++ test_windows_dockerfile.py
@@ -70,13 +70,15 @@ client = docker.from_env()
 
 # distro, shell = 'windows', 'cmd'
 
-# with running_docker_container(client, distro):
-#     with make_paramiko_repr(distro, shell) as paramiko_repr:
-#         output = paramiko_repr('"Hello world"')
-#         with open('test.json', 'wt') as f:
-#             json.dump(f, {"paramiko_repr_output": output})
+distro, shell = 'alpine', 'dash'
 
-with make_paramiko_repr('windows', 'ssh', 'Passw0rd') as paramiko_repr:
-    output = paramiko_repr('"Hello world"')
-    with open('test.json', 'wt') as f:
-        json.dump(f, {"paramiko_repr_output": output})
+with running_docker_container(client, distro):
+    with make_paramiko_repr(distro, shell, shell) as paramiko_repr:
+        output = paramiko_repr('"Hello world"')
+        with open('test.json', 'wt') as f:
+            json.dump(f, {"paramiko_repr_output": output})
+
+# with make_paramiko_repr('windows', 'ssh', 'Passw0rd') as paramiko_repr:
+#     output = paramiko_repr('"Hello world"')
+#     with open('test.json', 'wt') as f:
+#         json.dump(f, {"paramiko_repr_output": output})
