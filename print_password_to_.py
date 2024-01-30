15cd4261fcadd32787e5fca1fe5b4c04a28485bd
JamesParrott <80779630+JamesParrott@users.noreply.github.com>
2024-01-30 18:41:11 +0000

    Correct typo and don't add punctuation chars to password, so as not to mess up command line

--- print_password_to_.py
+++ print_password_to_.py
@@ -2,5 +2,5 @@ import secrets
 import string
 
 
-chars=string.printable.strip()
+chars=(string.ascii_letters + string.digits)
 print(f"password={''.join(secrets.choice(chars) for __ in range(30))}")
