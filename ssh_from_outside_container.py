fea41b81ba57cdc88dae948aea53ada03c83fd3e
JamesParrott <80779630+JamesParrott@users.noreply.github.com>
2024-01-31 17:36:21 +0000

    Run a cmd command, not powershell only ones.

--- ssh_from_outside_container.py
+++ ssh_from_outside_container.py
@@ -133,7 +133,8 @@ with fabric.Connection(
         port = port,
         connect_kwargs ={'password' : password}
         ) as c:
-    print(c.run('$PSVersionTable'))
+    # print(c.run('$PSVersionTable'))
     # print(c.run(f'echo %{env_var}%'))
     # print(c.run(f'Write-Host $env:{env_var}'))
-    print(c.run(f'Write-Host "Hello_World"'))
+    # print(c.run(f'Write-Host "Hello_World"'))
+    print(c.run(f'echo Hello World!'))
