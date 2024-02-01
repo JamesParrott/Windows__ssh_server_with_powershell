3f9c869e7ade16fc8f44b576977ef4f1f7831dd9
JamesParrott <80779630+JamesParrott@users.noreply.github.com>
2024-02-01 16:39:36 +0000

    Remove env var (it's probably only readable as admin, not as user ssh)

--- ssh_from_outside_container.py
+++ ssh_from_outside_container.py
@@ -3,7 +3,7 @@ import sys
 
 import fabric
 
-username, password, port, env_var = sys.argv[1:5]
+username, password, port = sys.argv[1:4]
 
 with fabric.Connection(
         'localhost',
@@ -11,16 +11,23 @@ with fabric.Connection(
         port = port,
         connect_kwargs ={'password' : password}
         ) as c:
-    print('CMD or PSVersionTable: Shell is: ',
-                # https://stackoverflow.com/a/61469226/20785734
-          c.run('(dir 2>&1 *`|echo CMD);&<# rem #>echo $PSVersionTable'),
-                # Returns one of: CMD, Core, Desktop}')
-          )  
+    
+    # https://stackoverflow.com/a/61469226/20785734
+    c.run('(dir 2>&1 *`|echo CMD);&<# rem #>echo $PSVersionTable'),
+                # Returns:CMD or $PSVersionTable
     
     # Alternatives if using cmd as login shell
-    #     print(c.run(f'echo %{env_var}%'))
     #     print(c.run(f'echo Hello World!'))
     
-    print(c.run(f'Write-Host $env:{env_var}'))
     print(c.run('Write-Host "Hello_World"'))
+#     print('CMD or PSVersionTable: Shell is: ',
+#                 # https://stackoverflow.com/a/61469226/20785734
+#           c.run('(dir 2>&1 *`|echo CMD);&<# rem #>echo $PSVersionTable'),
+#                 # Returns one of: CMD, Core, Desktop}')
+#           )  
+    
+#     # Alternatives if using cmd as login shell
+#     #     print(c.run(f'echo Hello World!'))
+    
+#     print(c.run('Write-Host "Hello_World"'))
 
